6ae2073ea51db197e5652a2f0811e0b9092f0086
Matt Carter <matt@showdeo.com>
2017-03-31 11:32:28 -0700

    fixes tooltips on mobile

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -20,6 +20,27 @@ const styles = {
   }
 }
 
+const isMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+};
+
 class Projects extends Component {
   constructor(props) {
     super(props);
@@ -38,7 +59,9 @@ class Projects extends Component {
             classy={project.class}
             tooltip={project.name}
           />
-          <ReactTooltip type="info" />
+          <ReactTooltip
+            globalEventOff={isMobile.any() ? 'click' : undefined}
+            type="info" />
           <ProjectDetail
             classy={project.detailClass}
             click={() => this.restoreProjects()}
